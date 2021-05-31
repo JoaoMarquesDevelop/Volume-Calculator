a1a7cb5ad43808f7b15837867046e9230ab2c900
Isle New Computer <isletowerdefense@gmail.com>
2021-05-31 17:28:55 +0200

    first commit

---
+++ src/main/java/com/crx/volumecalculator/VolumeCalculatorApplication.java
@@ -0,0 +1,13 @@
+package com.crx.volumecalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VolumeCalculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VolumeCalculatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/crx/volumecalculator/VolumeCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crx.volumecalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VolumeCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
